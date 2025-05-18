ddf554410b3842ada0224082bc305ebc77bd01f7
Malaydewangan09 <malaydewangan310@gmail.com>
2025-05-18 23:54:59 +0530

    feat: add Ollama CLI task for Ollama plugin

---
+++ src/main/java/io/kestra/plugin/ollama/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains general tasks for working with Ollama models and features.",
+    categories = PluginSubGroup.PluginCategory.AI
+)
+package io.kestra.plugin.ollama;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
---
+++ src/test/java/io/kestra/plugin/ollama/OllamaCLITest.java
@@ -0,0 +1,47 @@
+package io.kestra.plugin.ollama;
+
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.runners.TaskRunnerDetailResult;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.ollama.cli.OllamaCLI;
+import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
+import io.kestra.plugin.scripts.runner.docker.Docker;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+
+@KestraTest
+class OllamaCLITest {
+    @Inject
+    private RunContextFactory runContextFactory;
+
+    @Test
+    void shouldExecuteCommandsAndReturnSuccessExitCode() throws Exception {
+        OllamaCLI task = OllamaCLI.builder()
+            .id(OllamaCLI.class.getSimpleName())
+            .type(OllamaCLI.class.getName())
+            .commands(Property.of(List.of(
+                "ollama pull smollm:360m",
+                "ollama run smollm:360m \"What is your name?\" > output.txt"
+            )))
+            .containerImage(Property.of("alpine/ollama"))
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, task, Map.of());
+
+        var output = task.run(runContext);
+
+        assertThat(output, notNullValue());
+        assertThat(output.getExitCode(), is(0));
+    }
+}
