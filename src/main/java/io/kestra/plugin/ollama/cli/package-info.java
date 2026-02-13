2a00135133f2e587fc3ac4f3ceeb7ffdf000dcf7
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:33:48 +0100

    chore: update plugin categories (#45)

--- src/main/java/io/kestra/plugin/ollama/cli/package-info.java
+++ src/main/java/io/kestra/plugin/ollama/cli/package-info.java
@@ -1,7 +1,8 @@
 @PluginSubGroup(
     title = "CLI",
-    description = "This sub-group of plugins contains tasks for interacting with the Ollama CLI.",
-    categories = PluginSubGroup.PluginCategory.AI
+    description = "This sub-group of plugins contains tasks for interacting with the Ollama CLI.",categories = {
+        PluginSubGroup.PluginCategory.AI
+    }
 )
 package io.kestra.plugin.ollama.cli;
 
--- src/main/java/io/kestra/plugin/ollama/package-info.java
+++ src/main/java/io/kestra/plugin/ollama/package-info.java
@@ -1,6 +1,7 @@
 @PluginSubGroup(
-    description = "This sub-group of plugins contains general tasks for working with Ollama models and features.",
-    categories = PluginSubGroup.PluginCategory.AI
+    description = "This sub-group of plugins contains general tasks for working with Ollama models and features.",categories = {
+        PluginSubGroup.PluginCategory.AI
+    }
 )
 package io.kestra.plugin.ollama;
 
